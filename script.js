0f07a27e3c3b5b07977b44bf1d57b14b0d8179fb
Kristen Krofchik <k.krofchik@gmail.com>
2020-09-27 01:48:35 -0500

    added css styling, lost some functionality

--- script.js
+++ script.js
@@ -1,24 +1,24 @@
 const gameContainer = document.getElementById("game");
+const allDivs = document.querySelectorAll(".flipped");
 let cardsFlipped = 0;
 let card1 = null;
 let card2 = null;
 let noClicking = false;
-let gamePlaying = false;
-let startButton = document.querySelector('#startButton');
+//let gamePlaying = false;
+//let startButton = document.querySelector('#startButton');
 
-const COLORS = [
-  "red",
-  "blue",
-  "green",
-  "orange",
-  "purple",
-  "red",
-  "blue",
-  "green",
-  "orange",
-  "purple"
+const CANDY = [
+'m&m',
+'brachs',
+'lifesavers', 
+'milkyway', 
+'wrigleysgum',
+'m&m',
+'brachs',
+'lifesavers', 
+'milkyway', 
+'wrigleysgum'
 ];
-
 
 function shuffle(array) {
   let counter = array.length;
@@ -32,14 +32,14 @@ function shuffle(array) {
   return array;
 }
 
-let shuffledColors = shuffle(COLORS);
+let shuffledCandy = shuffle(CANDY);
 
 
-function createDivsForColors(colorArray) {
-  for (let color of colorArray) {
+function createDivsForCandy(candyArray) {
+  for (let candy of candyArray) {
     const newDiv = document.createElement("div");
-    newDiv.classList.add(color);
-    newDiv.style.backgroundColor = 'white';
+    newDiv.classList.add(candy);
+    newDiv.classList.add("notFlipped");
     newDiv.addEventListener("click", handleCardClick);
     gameContainer.append(newDiv);
   }
@@ -49,28 +49,26 @@ function createDivsForColors(colorArray) {
 
 
 function handleCardClick(event) { 
-  if(noClicking) {
-    return;
-  }; //cannot click
-  
-  //if(e.target.classList.contains("flipped")) {
-   // return;
-  //}; //already flipped a match, cannot click again
+  if (noClicking) return; //cannot click
+  if(event.target.classList.contains('flipped')) return;
 
   let currentCard = event.target;
-  currentCard.style.backgroundColor = currentCard.classList[0];
-  currentCard.classList.add('flip');
+  const imageURL = `url("images/${currentCard.classList[0]}.jpg")`
+  currentCard.style.backgroundImage = imageURL;
 
-  if (!card1 || !card2) {
+
+  if (card1 === null || card2 === null) {
     currentCard.classList.add("flipped");
-    card1 = card1 || currentCard; //if there is a card1, keep it. if not, make it the current card
-    card2 = currentCard === card1 ? null : currentCard; //card2 is currentCard if card1 is not
+    currentCard.classList.remove("notFlipped");
+    card1 = card1 || currentCard;
+    card2 = currentCard === card1 ? null : currentCard;
   }
+
 
 if (card1 && card2) {
     noClicking = true;
     let check1 = card1.classList[0];//check that they are the same color
-    let check2 = card2.classList[0];
+    let check2 = card2.classList;
 
     if (check1 === check2) {
       cardsFlipped += 2;
@@ -79,30 +77,23 @@ if (card1 && card2) {
       card1 = null;
       card2 = null;
       noClicking = false; //if the cards match
+  
   } else {
       setTimeout(function() {
+          card1.style.backgroundColor = "";
+          card2.style.backgroundColor = "";
           card1.classList.remove("flipped");
           card2.classList.remove("flipped");
-          card1.classList.add("flip-back");
-          card2.classList.add("flip-back");
-          card1.style.backgroundColor = "white";
-          card2.style.backgroundColor = "white";
+          card1.classList.add("notFlipped");
+          card2.classList.add("notFlipped");
           card1 = null;
           card2 = null;
           noClicking = false; //if the cards don't match
       }, 1000)
   }
 }
-  console.log("you just clicked", event.target);
+
 }
 
-
-
-
-
-  
-
-
 // when the DOM loads
-createDivsForColors(shuffledColors);
-
+createDivsForCandy(shuffledCandy);
